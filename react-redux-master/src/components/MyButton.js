7dc113506bcb0fcd8767aec01b5babb5621530f0
vikiru <72267229+vikiru@users.noreply.github.com>
2023-10-31 16:49:37 -0400

    Commit changes following lab instructions

--- react-redux-master/src/components/MyButton.js
+++ react-redux-master/src/components/MyButton.js
@@ -1,1 +1,10 @@
-
+import React from "react";
+import increment from "../actions";
+import { useDispatch } from "react-redux";
+const MyButton = () => {
+  let dispatch = useDispatch();
+  return (
+    <button onClick={() => dispatch(increment(1))}>Increase counter</button>
+  );
+};
+export default MyButton;
