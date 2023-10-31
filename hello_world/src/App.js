4bf07377ec45e9d8bd481957a8e798c20fd6f8ff
vikiru <72267229+vikiru@users.noreply.github.com>
2023-10-31 17:19:03 -0400

    Commit changes following lab instructions

--- hello_world/src/App.js
+++ hello_world/src/App.js
@@ -1,7 +1,13 @@
-function App() {
+function App(props) {
+  const currDate = new Date();
   return (
-      <h1>Hello World</h1>
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>
+        It is {currDate.toLocaleDateString()} and the time now is{" "}
+        {currDate.toLocaleTimeString()}.
+      </h2>
+    </div>
   );
 }
-
 export default App;
